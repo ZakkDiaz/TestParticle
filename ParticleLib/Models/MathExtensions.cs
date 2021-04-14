ecb119782f23d9b02caf25b701218d3a1531fa0e
Zakkery Diaz <zakkdiaz@hotmail.com>
2021-04-13 18:35:17 -0700

    Remove tuples

--- ParticleLib/Models/MathExtensions.cs
+++ ParticleLib/Models/MathExtensions.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace ParticleLib.Models
 {
-    public static class MathExtensions
-    {
-        public static float AngleFor((float x, float y) from, (float X, float Y) to)
-        {
-            return AngleFor((to.Item2 - from.Item2), (to.Item1 - from.Item1));
-        }
-        public static float AngleFor(float x, float y)
-        {
-            return (float)(Math.Atan2(y, x));
-        }
-    }
+    //public static class MathExtensions
+    //{
+    //    public static float AngleFor((float x, float y) from, (float X, float Y) to)
+    //    {
+    //        return AngleFor((to.Item2 - from.Item2), (to.Item1 - from.Item1));
+    //    }
+    //    public static float AngleFor(float x, float y)
+    //    {
+    //        return (float)(Math.Atan2(y, x));
+    //    }
+    //}
 }
